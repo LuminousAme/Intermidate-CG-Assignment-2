a2a51023fa31c38a55399da781b2728516e908cc
Ame <amegilham@gmail.com>
2021-02-06 19:23:37 -0500

    minor shader optimization

--- modules/Titan/res/shaders/Post/ttn_passthrough_frag.glsl
+++ modules/Titan/res/shaders/Post/ttn_passthrough_frag.glsl
@@ -16,7 +16,7 @@ void main()
 	//just sample the texture
 	vec4 source = texture(s_screenTex, inUV);
 
-	float alhpa = (source.a == 0.0) ? 0.0 : 1.0; 
+	float alhpa = (ceil(source.a));
 
 	//and apply it to the full screen quad
 	frag_color = vec4(source.rgb, alhpa);
